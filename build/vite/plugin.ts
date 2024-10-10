ff68c522669189d469610e715e7e179624949edd
HH <sunxiansheng99999@163.com>
2024-10-10 14:02:55 +0800

    init commit

--- build/vite/plugin.ts
+++ build/vite/plugin.ts
@@ -3,7 +3,6 @@ import { VantResolver } from "unplugin-vue-components/resolvers";
 import Components from "unplugin-vue-components/vite";
 import type { Plugin, PluginOption } from "vite";
 import { createHtmlPlugin } from "vite-plugin-html";
-import PagesPlugin from "vite-plugin-pages";
 import vueSetupExtend from "vite-plugin-vue-setup-extend";
 
 import vue from "@vitejs/plugin-vue";
@@ -17,24 +16,6 @@ export function createVitePlugins(viteEnv: ViteEnv) {
 
 		vueJsx(),
 
-		PagesPlugin({
-			// pagesDir: ["src/views",baseRoute: "/"], // 目录存储
-			dirs: [{ dir: "src/views", baseRoute: "/" }],
-			// dirs: "src/views",
-
-			// 排除在外的目录，即不将所有 components 目录下的 .vue 文件生成路由
-			exclude: ["**/components/*.vue"],
-			// 异步方式加载路由组件
-			importMode: "async",
-			// 启用 routeBlockLang，支持 <route lang="yaml"> 格式
-			routeBlockLang: "yaml",
-			// 遍历路由信息，给默认路由加一个redirect
-			extendRoute(route) {
-				if (route.path === "/") return { ...route };
-				console.log("当前行：", 33, "[ route ] ==>", route);
-			},
-			extensions: ["vue"] // 只希望处理 Vue 组件
-		}),
 		// vant 组件自动按需引入
 		Components({
 			dts: "types/components.d.ts",
--- src/views/asd/index.vue
+++ src/views/asd/index.vue
@@ -5,15 +5,3 @@
 <script setup lang="ts"></script>
 
 <style lang="scss" scoped></style>
-
-<route lang="yaml">
-name: Asd
-meta:
-  requiresAuth: true
-  title: 测试页面
-
-props:
-  id: $route.params.id
-  isSub: $route.query.isSub === "y"
-  isAudit: $route.query.isAudit === "y"
-</route>
