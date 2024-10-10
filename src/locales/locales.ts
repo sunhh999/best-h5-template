2ec23ec4b936027acbc7f5efe3fdac49ce3840b4
HH <sunxiansheng99999@163.com>
2024-10-10 14:03:40 +0800

    init commit

--- src/locales/locales.ts
+++ src/locales/locales.ts
@@ -1,9 +1,9 @@
-import zhCNLocale from "./lang/zh-CN.json";
 import enLocale from "./lang/en.json";
+import zhCNLocale from "./lang/zh-CN.json";
 
 const locales = {
-  "zh-cn": zhCNLocale,
-  en: enLocale
+	"zh-cn": zhCNLocale,
+	en: enLocale
 };
 
 export default locales;
